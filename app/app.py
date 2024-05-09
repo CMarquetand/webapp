f5be024794864af0a480d525b6be793976682b42
CMarquetand <102695944+CMarquetand@users.noreply.github.com>
2024-05-09 17:42:24 +0200

    fix pylint warnings

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 import os
 import datetime
 import hashlib
-from flask import (Flask, session, url_for, redirect, render_template, 
+from flask import (Flask, session, url_for, redirect, render_template,
                    request, abort, flash)
 from database import (list_users, verify, delete_user_from_db, add_user,
-                      read_note_from_db, write_note_into_db, 
+                      read_note_from_db, write_note_into_db,
                       delete_note_from_db, match_user_id_with_note_id)
-from database import (image_upload_record, list_images_for_user, 
+from database import (image_upload_record, list_images_for_user,
                       match_user_id_with_image_uid, delete_image_from_db)
 from werkzeug.utils import secure_filename
 
@@ -17,27 +17,27 @@ app.config.from_object('config')
 
 
 @app.errorhandler(401)
-def fun_401(error):
+def fun_401(_error):
     '''error 401'''
     return render_template("page_401.html"), 401
 
 @app.errorhandler(403)
-def fun_403(error):
+def fun_403(_error):
     '''error 403'''
     return render_template("page_403.html"), 403
 
 @app.errorhandler(404)
-def fun_404(error):
+def fun_404(_error):
     '''error 404'''
     return render_template("page_404.html"), 404
 
 @app.errorhandler(405)
-def fun_405(error):
+def fun_405(_error):
     '''error 405'''
     return render_template("page_405.html"), 405
 
 @app.errorhandler(413)
-def fun_413(error):
+def fun_413(_error):
     '''error 413'''
     return render_template("page_413.html"), 413
 
@@ -70,10 +70,8 @@ def fun_private():
                           [x[1] for x in images_list],\
                           [x[2] for x in images_list],\
                           ["/delete_image/" + x[0] for x in images_list])
-
         return render_template("private_page.html", notes = notes_table, images = images_table)
-    else:
-        return abort(401)
+    return abort(401)
 
 @app.route("/admin/")
 def fun_admin():
@@ -84,8 +82,7 @@ def fun_admin():
                         user_list,\
                         [x + y for x,y in zip(["/delete_user/"] * len(user_list), user_list)])
         return render_template("admin.html", users = user_table)
-    else:
-        return abort(401)
+    return abort(401)
 
 
 
@@ -103,7 +100,7 @@ def fun_write_note():
 @app.route("/delete_note/<note_id>", methods = ["GET"])
 def fun_delete_note(note_id):
     '''function to delete a note'''
-    if session.get("current_user", None) == match_user_id_with_note_id(note_id): 
+    if session.get("current_user", None) == match_user_id_with_note_id(note_id):
         delete_note_from_db(note_id)
     else:
         return abort(401)
@@ -146,11 +143,13 @@ def fun_upload_image():
 def fun_delete_image(image_uid):
     '''delete_image'''
         # Ensure the current user is NOT operating on other users' note.
-    if session.get("current_user", None) == match_user_id_with_image_uid(image_uid): 
+    if session.get("current_user", None) == match_user_id_with_image_uid(image_uid):
         # delete the corresponding record in database
         delete_image_from_db(image_uid)
         # delete the corresponding image file from image pool
-        image_to_delete_from_pool = [y for y in [x for x in os.listdir(app.config['UPLOAD_FOLDER'])] if y.split("-", 1)[0] == image_uid][0]
+        image_to_delete_from_pool = [
+          y for y in [x for x in os.listdir(app.config['UPLOAD_FOLDER'])]
+          if y.split("-", 1)[0] == image_uid][0]
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], image_to_delete_from_pool))
     else:
         return abort(401)
@@ -165,13 +164,13 @@ def fun_login():
     if (id_submitted in list_users()) and verify(id_submitted, request.form.get("pw")):
         session['current_user'] = id_submitted
     
-    return(redirect(url_for("fun_root")))
+    return redirect(url_for("fun_root"))
 
 @app.route("/logout/")
 def fun_logout():
     '''logout'''
     session.pop("current_user", None)
-    return(redirect(url_for("fun_root")))
+    return redirect(url_for("fun_root"))
 
 @app.route("/delete_user/<id>/", methods = ['GET'])
 def fun_delete_user(id):
@@ -183,36 +182,37 @@ def fun_delete_user(id):
         # [1] Delete this user's images in image pool
         images_to_remove = [x[0] for x in list_images_for_user(id)]
         for f in images_to_remove:
-            image_to_delete_from_pool = [y for y in [x for x in os.listdir(app.config['UPLOAD_FOLDER'])] if y.split("-", 1)[0] == f][0]
+            image_to_delete_from_pool = [
+              y for y in [x for x in os.listdir(app.config['UPLOAD_FOLDER'])]
+              if y.split("-", 1)[0] == f][0]
             os.remove(os.path.join(app.config['UPLOAD_FOLDER'], image_to_delete_from_pool))
         # [2] Delele the records in database files
         delete_user_from_db(id)
-        return(redirect(url_for("fun_admin")))
-    else:
-        return abort(401)
+        return redirect(url_for("fun_admin"))
+    return abort(401)
 
 @app.route("/add_user", methods = ["POST"])
 def fun_add_user():
     '''add user'''
     if session.get("current_user", None) == "ADMIN": # only Admin should be able to add user.
-        # before we add the user, we need to ensure this is doesn't exsit in database. We also need to ensure the id is valid.
+        # before we add the user, we need to ensure this is doesn't exsit in database. 
+        # We also need to ensure the id is valid.
         if request.form.get('id').upper() in list_users():
             user_list = list_users()
             user_table = zip(range(1, len(user_list)+1),\
                             user_list,\
-                            [x + y for x,y in zip(["/delete_user/"] * len(user_list), user_list)])
+                            [x + y for x,y in zip(["/delete_user/"] 
+                            * len(user_list), user_list)])
             return(render_template("admin.html", id_to_add_is_duplicated = True, users = user_table))
         if " " in request.form.get('id') or "'" in request.form.get('id'):
             user_list = list_users()
             user_table = zip(range(1, len(user_list)+1),\
                             user_list,\
                             [x + y for x,y in zip(["/delete_user/"] * len(user_list), user_list)])
-            return(render_template("admin.html", id_to_add_is_invalid = True, users = user_table))
-        else:
-            add_user(request.form.get('id'), request.form.get('pw'))
-            return(redirect(url_for("fun_admin")))
-    else:
-        return abort(401)
+            return render_template("admin.html", id_to_add_is_invalid = True, users = user_table)
+        add_user(request.form.get('id'), request.form.get('pw'))
+        return redirect(url_for("fun_admin"))
+    return abort(401)
 
 
 
