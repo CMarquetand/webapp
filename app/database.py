638b1a7f6b7c42b90c6e1a5af2be7194eb5cce96
CMarquetand <102695944+CMarquetand@users.noreply.github.com>
2024-05-09 18:13:46 +0200

    fix pylint warnings

--- app/database.py
+++ app/database.py
@@ -1,75 +1,83 @@
+''' database functions '''
 import sqlite3
 import hashlib
 import datetime
 
-user_db_file_location = "database_file/users.db"
-note_db_file_location = "database_file/notes.db"
-image_db_file_location = "database_file/images.db"
+USER_DB_FILE_LOCATION = "database_file/users.db"
+NOTE_DB_FILE_LOCATION = "database_file/notes.db"
+IMAGE_DB_FILE_LOCATION = "database_file/images.db"
 
 def list_users():
     '''list user'''
-    _conn = sqlite3.connect(user_db_file_location)
+    _conn = sqlite3.connect(USER_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     _c.execute("SELECT id FROM users;")
     result = [x[0] for x in _c.fetchall()]
 
     _conn.close()
-    
+
     return result
 
-def verify(id, pw):
-    '''verify user'''
-    _conn = sqlite3.connect(user_db_file_location)
+def verify(user_id, password):
+    '''Verify user'''
+    _conn = sqlite3.connect(USER_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
-    _c.execute("SELECT pw FROM users WHERE id = '" + id + "';")
-    result = _c.fetchone()[0] == hashlib.sha256(pw.encode()).hexdigest()
-    
+    # Use parameterized queries to prevent SQL injection
+    query = "SELECT pw FROM users WHERE id = ?;"
+    _c.execute(query, (user_id,))
+
+    # Securely compare the stored password with the provided password's hash
+    result = _c.fetchone()
+    if result:
+        stored_password = result[0]
+        provided_password_hash = hashlib.sha256(password.encode()).hexdigest()
+        return stored_password == provided_password_hash
     _conn.close()
 
-    return result
+    return False
 
-def delete_user_from_db(id):
+def delete_user_from_db(user_id):
     '''delete user'''
-    _conn = sqlite3.connect(user_db_file_location)
+    _conn = sqlite3.connect(USER_DB_FILE_LOCATION)
     _c = _conn.cursor()
-    _c.execute("DELETE FROM users WHERE id = ?;", (id))
+    _c.execute("DELETE FROM users WHERE id = ?;", (user_id))
     _conn.commit()
     _conn.close()
 
     # when we delete a user FROM database USERS, we also need to delete all his or her notes data FROM database NOTES
-    _conn = sqlite3.connect(note_db_file_location)
+    _conn = sqlite3.connect(NOTE_DB_FILE_LOCATION)
     _c = _conn.cursor()
-    _c.execute("DELETE FROM notes WHERE user = ?;", (id))
+    _c.execute("DELETE FROM notes WHERE user = ?;", (user_id))
     _conn.commit()
     _conn.close()
 
-    # when we delete a user FROM database USERS, we also need to 
+    # when we delete a user FROM database USERS, we also need to
     # [1] delete all his or her images FROM image pool (done in app.py)
     # [2] delete all his or her images records FROM database IMAGES
-    _conn = sqlite3.connect(image_db_file_location)
+    _conn = sqlite3.connect(IMAGE_DB_FILE_LOCATION)
     _c = _conn.cursor()
-    _c.execute("DELETE FROM images WHERE owner = ?;", (id))
+    _c.execute("DELETE FROM images WHERE owner = ?;", (user_id))
     _conn.commit()
     _conn.close()
 
-def add_user(id, pw):
+def add_user(user_id, pw):
     '''add user'''
-    _conn = sqlite3.connect(user_db_file_location)
+    _conn = sqlite3.connect(USER_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
-    _c.execute("INSERT INTO users values(?, ?)", (id.upper(), hashlib.sha256(pw.encode()).hexdigest()))
-    
+    _c.execute("INSERT INTO users values(?, ?)", (user_id.upper(), hashlib.sha256(pw.encode()).hexdigest()))
+
     _conn.commit()
     _conn.close()
 
-def read_note_from_db(id):
+def read_note_from_db(user_id):
     '''read note from db'''
-    _conn = sqlite3.connect(note_db_file_location)
+    _conn = sqlite3.connect(NOTE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
-    command = "SELECT note_id, timestamp, note FROM notes WHERE user = '" + id.upper() + "';" 
+    command = "SELECT note_id, timestamp, note FROM notes WHERE user = '" + user_id.upper() + "';"
     _c.execute(command)
     result = _c.fetchall()
 
@@ -80,7 +88,7 @@ def read_note_from_db(id):
 
 def match_user_id_with_note_id(note_id):
     ''' Given the note id, confirm if the current user is the owner of the note which is being operated.'''
-    _conn = sqlite3.connect(note_db_file_location)
+    _conn = sqlite3.connect(NOTE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     command = "SELECT user FROM notes WHERE note_id = '" + note_id + "';" 
@@ -92,20 +100,22 @@ def match_user_id_with_note_id(note_id):
 
     return result
 
-def write_note_into_db(id, note_to_write):
+def write_note_into_db(user_id, note_to_write):
     '''write note into db'''
-    _conn = sqlite3.connect(note_db_file_location)
+    _conn = sqlite3.connect(NOTE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     current_timestamp = str(datetime.datetime.now())
-    _c.execute("INSERT INTO notes values(?, ?, ?, ?)", (id.upper(), current_timestamp, note_to_write, hashlib.sha1((id.upper() + current_timestamp).encode()).hexdigest()))
+    _c.execute("INSERT INTO notes values(?, ?, ?, ?)",\
+            (user_id.upper(), current_timestamp, note_to_write,\
+            hashlib.sha1((id.upper() + current_timestamp).encode()).hexdigest()))
 
     _conn.commit()
     _conn.close()
 
 def delete_note_from_db(note_id):
     '''delete note from db'''
-    _conn = sqlite3.connect(note_db_file_location)
+    _conn = sqlite3.connect(NOTE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     _c.execute("DELETE FROM notes WHERE note_id = ?;", (note_id))
@@ -115,7 +125,7 @@ def delete_note_from_db(note_id):
 
 def image_upload_record(uid, owner, image_name, timestamp):
     '''image upload'''
-    _conn = sqlite3.connect(image_db_file_location)
+    _conn = sqlite3.connect(IMAGE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     _c.execute("INSERT INTO images VALUES (?, ?, ?, ?)", (uid, owner, image_name, timestamp))
@@ -125,7 +135,7 @@ def image_upload_record(uid, owner, image_name, timestamp):
 
 def list_images_for_user(owner):
     '''list images from user'''
-    _conn = sqlite3.connect(image_db_file_location)
+    _conn = sqlite3.connect(IMAGE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     command = "SELECT uid, timestamp, name FROM images WHERE owner = '{0}'".format(owner)
@@ -139,10 +149,10 @@ def list_images_for_user(owner):
 
 def match_user_id_with_image_uid(image_uid):
     '''Given the note id, confirm if the current user is the owner of the note which is being operated.'''
-    _conn = sqlite3.connect(image_db_file_location)
+    _conn = sqlite3.connect(IMAGE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
-    command = "SELECT owner FROM images WHERE uid = '" + image_uid + "';" 
+    command = "SELECT owner FROM images WHERE uid = '" + image_uid + "';"
     _c.execute(command)
     result = _c.fetchone()[0]
 
@@ -153,7 +163,7 @@ def match_user_id_with_image_uid(image_uid):
 
 def delete_image_from_db(image_uid):
     '''delete image from db'''
-    _conn = sqlite3.connect(image_db_file_location)
+    _conn = sqlite3.connect(IMAGE_DB_FILE_LOCATION)
     _c = _conn.cursor()
 
     _c.execute("DELETE FROM images WHERE uid = ?;", (image_uid))
